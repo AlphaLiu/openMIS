f78edac849febf21e9c35c7c70939def92bdad87
Alpha Liu <alpha.liu@me.com>
2012-11-06 22:24:51 +0800

    add user view (2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
+#encoding:utf-8
 class UsersController < ApplicationController
-  def new
-  end
 
-  def show
-  	@user = User.find(params[:id])
-  end
+	def index
+		@users = User.all ##use 'all' is a risk, fix it later.
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "用户 #{@user.name} 创建成功！"
+      		redirect_to users_path
+	  else
+	  	render 'new'
+	  end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,11 +11,32 @@ describe "UserPages" do
 		it { should have_selector('title', text: full_title('创建新用户')) }
 	end
 
-	describe "profile page" do
-		let(:user) { FactoryGirl.create(:user) }
-		before { visit user_path(user) }
+	describe "signup" do
 
-		it { should have_selector('h1',    text: user.name) }
-		it { should have_selector('title', text: user.name) }
+		before { visit signup_path }
+
+		let(:submit) { "创建用户" }
+
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "用户名",         with: "Example User"
+				fill_in "电子邮箱",        with: "user@example.com"
+				fill_in "密码",     with: "foobar"
+				fill_in "确认密码", with: "foobar"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+		end
 	end
+
+
 end
