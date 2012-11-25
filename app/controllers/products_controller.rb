fb962e1b8d0cbc207fea212dc66489b4bba6dc77
Alpha Liu <alpha.liu@me.com>
2012-11-25 22:40:33 +0800

    add ajax search product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,7 @@ class ProductsController < ApplicationController
   def searchview
   end
   def search
+    @name = params[:name]
+    #@products = Products.where("product_name like ?", "%#{params[:name]}%")
   end
 end
